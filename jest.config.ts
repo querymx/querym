6a2d4ade5d0c65f53b30e07f9a4f9177ab46822f
Visal .In <invisal@gmail.com>
2023-05-16 08:04:56 +0700

    refactor: move jest config out from package.json

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleDirectories: ['node_modules', 'release/app/node_modules', 'src'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.erb/mocks/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
+  testPathIgnorePatterns: ['release/app/dist', '.erb/dll'],
+  transform: {
+    '\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['./src/**/*.ts'],
+};
+
+export default config;
