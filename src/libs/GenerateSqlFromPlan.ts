8a8553ada1a412e98b87811def3ac4ccf499d876
Visal .In <invisal@gmail.com>
2023-06-10 21:25:37 +0700

    fix: new query builder does not support json type

--- src/libs/GenerateSqlFromPlan.ts
+++ src/libs/GenerateSqlFromPlan.ts
@@ -1,10 +1,23 @@
 import { SqlStatementPlan } from 'types/SqlStatement';
 import { QueryBuilder } from './QueryBuilder';
 
+function convertUnsupportedValue(value: Record<string, unknown>) {
+  const result = { ...value };
+  for (const key of Object.keys(result)) {
+    if (typeof result[key] === 'object') {
+      result[key] = JSON.stringify(result[key]);
+    }
+  }
+
+  return result;
+}
+
 export default function generateSqlFromPlan(plan: SqlStatementPlan) {
   if (plan.type === 'update') {
-    const qb = new QueryBuilder('mysql').table(plan.table).update(plan.values);
-    if (plan.where) qb.where(plan.where);
+    const qb = new QueryBuilder('mysql')
+      .table(plan.table)
+      .update(convertUnsupportedValue(plan.values));
+    if (plan.where) qb.where(convertUnsupportedValue(plan.where));
     return qb.toRawSQL();
   }
   return '';
