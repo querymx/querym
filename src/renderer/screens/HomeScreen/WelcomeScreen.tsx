67b89dfac3554bde6b39bdc5066ae60498a744a2
Visal .In <invisal@gmail.com>
2023-06-14 10:40:42 +0700

    bump to version 0.1.3 and add proper logo

--- src/renderer/screens/HomeScreen/WelcomeScreen.tsx
+++ src/renderer/screens/HomeScreen/WelcomeScreen.tsx
@@ -1,10 +1,13 @@
 import Stack from 'renderer/components/Stack';
 import Heading from 'renderer/components/Typo/Heading';
+import imageLogo from './../../../../assets/icon.svg';
 import pkg from './../../../../package.json';
 
 export default function WelcomeScreen() {
   return (
     <Stack padding vertical full center>
+      <img src={imageLogo} alt="" width={150} height={150} />
+
       <Heading>Query Master v{pkg.version}</Heading>
 
       <p>
