399623746f92f4c6c1dc389eadb0d9e982b20d09
Visal .In <invisal@gmail.com>
2023-07-07 09:09:50 +0700

    feat: unique name for each new query window by incrementing the number

--- src/renderer/utils/generateDatabaseName.ts
+++ src/renderer/utils/generateDatabaseName.ts
@@ -1,15 +1,12 @@
 import { ConnectionStoreItem } from 'drivers/SQLLikeConnection';
+import generateIncrementalName from './generateIncrementalName';
 
 export default function generateDatabaseName(
   dbs: ConnectionStoreItem[],
   name: string
 ) {
-  const finalName = name.replace(/ \(\d+\)$/g, '');
-
-  for (let i = 0; i < 100; i++) {
-    const newName = i > 0 ? `${finalName} (${i})` : finalName;
-    if (!dbs.find((db) => db.name === newName)) return newName;
-  }
-
-  return '';
+  return generateIncrementalName(
+    dbs.map((db) => db.name),
+    name
+  );
 }
