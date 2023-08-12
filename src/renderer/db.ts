f15ddbe257bd87ebdec0520aa0e711c7b7980ea9
rin-yato <chearithorn@gmail.com>
2023-08-13 00:31:27 +0700

    feat: added keybinding table

--- src/renderer/db.ts
+++ src/renderer/db.ts
@@ -1,8 +1,25 @@
 import Dexie from 'dexie';
+import { keybindingsDefault } from './hooks/useKeybindings';
 
 export const db = new Dexie('query-master');
 
 db.version(1).stores({
   database_config: 'id',
   database_tabs: 'id',
+  keybindings: 'id',
 });
+
+db.on('populate', () => {
+  // auto populate default keybindings
+  populateDefaultKeybindings();
+});
+
+async function populateDefaultKeybindings() {
+  const existingKeybindings = await db.table('keybindings').get(1);
+  if (!existingKeybindings) {
+    await db.table('keybindings').add({
+      id: 1,
+      keybindings: keybindingsDefault,
+    });
+  }
+}
