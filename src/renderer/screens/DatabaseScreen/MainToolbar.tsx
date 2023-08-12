eb9beb10d92b9af3a3dc5f1c9e2f137505ac60b8
rin-yato <chearithorn@gmail.com>
2023-08-13 00:34:20 +0700

    feat: added setting and keybinding into toolbar

--- src/renderer/screens/DatabaseScreen/MainToolbar.tsx
+++ src/renderer/screens/DatabaseScreen/MainToolbar.tsx
@@ -1,43 +1,76 @@
-import { faPlugCircleXmark, faShield } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlugCircleXmark,
+  faShield,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 import { useConnection } from 'renderer/App';
+import { ChildWindow } from 'renderer/components/ChildWindow';
+import Keybindings from 'renderer/components/Keybindings';
 import Toolbar from 'renderer/components/Toolbar';
 import { useDatabaseSetting } from 'renderer/contexts/DatabaseSettingProvider';
 
 export default function MainToolbar() {
   const { disconnect } = useConnection();
   const { protectionLevel, setProductionLevel } = useDatabaseSetting();
+  const [keybindingsWindowOpen, setKeybindingsWindowOpen] =
+    React.useState(false);
 
   return (
-    <Toolbar shadow>
-      <Toolbar.Item
-        text="Disconnect"
-        icon={<FontAwesomeIcon icon={faPlugCircleXmark} />}
-        onClick={() => {
-          disconnect();
-        }}
-      />
-      <Toolbar.ContextMenu
-        text={`Protection: Level ${protectionLevel}`}
-        icon={<FontAwesomeIcon icon={faShield} />}
-        items={[
-          {
-            text: 'Level 0 - Send queries without any warning',
-            tick: protectionLevel === 0,
-            onClick: () => setProductionLevel(0),
-          },
-          {
-            text: 'Level 1 - Warn before send queries except SELECT',
-            tick: protectionLevel === 1,
-            onClick: () => setProductionLevel(1),
-          },
-          {
-            text: 'Level 2 - Warn before send queries',
-            tick: protectionLevel === 2,
-            onClick: () => setProductionLevel(2),
-          },
-        ]}
-      />
-    </Toolbar>
+    <React.Fragment>
+      <Toolbar shadow>
+        <Toolbar.Item
+          text="Disconnect"
+          icon={<FontAwesomeIcon icon={faPlugCircleXmark} />}
+          onClick={() => {
+            disconnect();
+          }}
+        />
+        <Toolbar.ContextMenu
+          text={`Protection: Level ${protectionLevel}`}
+          icon={<FontAwesomeIcon icon={faShield} />}
+          items={[
+            {
+              text: 'Level 0 - Send queries without any warning',
+              tick: protectionLevel === 0,
+              onClick: () => setProductionLevel(0),
+            },
+            {
+              text: 'Level 1 - Warn before send queries except SELECT',
+              tick: protectionLevel === 1,
+              onClick: () => setProductionLevel(1),
+            },
+            {
+              text: 'Level 2 - Warn before send queries',
+              tick: protectionLevel === 2,
+              onClick: () => setProductionLevel(2),
+            },
+          ]}
+        />
+        <Toolbar.ContextMenu
+          text="Settings"
+          icon={<FontAwesomeIcon icon={faGear} />}
+          items={[
+            {
+              text: 'Keybingings',
+              onClick: () => {
+                console.log('first');
+                setKeybindingsWindowOpen(true);
+              },
+            },
+          ]}
+        />
+      </Toolbar>
+
+      {keybindingsWindowOpen && (
+        <ChildWindow
+          options="width=600, height=600, alwaysOnTop=true"
+          onClosed={() => setKeybindingsWindowOpen(false)}
+        >
+          <Keybindings />
+        </ChildWindow>
+      )}
+    </React.Fragment>
   );
 }
