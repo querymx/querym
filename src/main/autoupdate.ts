750dc6af08d5350470def432500acffdbb0f8dbe
Visal .In <invisal@gmail.com>
2023-09-09 21:13:20 +0700

    feat: don't check for auto update for window

--- src/main/autoupdate.ts
+++ src/main/autoupdate.ts
@@ -31,7 +31,15 @@ export default function handleAutoUpdate(window: BrowserWindow) {
   ipcMain.handle('check-for-updates', () => {
     if (globalCheckForUpdate) return;
     globalCheckForUpdate = true;
-    autoUpdater.checkForUpdates();
+
+    // DO NOT CHECK FOR UPDATE FOR WINDOW
+    // Because the window certificate is too expensive,
+    // we will only build appx and upload it to Microsoft Store
+    // It is only $19 one-time. The auto updater does not work
+    // in Microsoft Store
+    if (!process.platform.startsWith('win')) {
+      autoUpdater.checkForUpdates();
+    }
   });
 
   ipcMain.handle('quit-and-install', () => {
