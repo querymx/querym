995f64865325a54d2bed67cc5ce27c4f9fa227a8
Visal .In <invisal@gmail.com>
2023-10-05 20:31:52 +0700

    fix: fixing the postgre limit query builder (#157)

--- src/libs/QueryBuilder.test.ts
+++ src/libs/QueryBuilder.test.ts
@@ -8,7 +8,7 @@ test('Test update statement with condition', () => {
       .table('users')
       .update({ name: 'query-master' })
       .where({ id: 5 })
-      .toRawSQL()
+      .toRawSQL(),
   ).toBe("UPDATE `users` SET `name`='query-master' WHERE `id`=5;");
 });
 
@@ -20,7 +20,7 @@ test('Test update statement with multiple condition', () => {
       .table('users')
       .update({ name: 'query-master' })
       .where({ id: 5, age: 20 })
-      .toRawSQL()
+      .toRawSQL(),
   ).toBe("UPDATE `users` SET `name`='query-master' WHERE `id`=5 AND `age`=20;");
 });
 
@@ -32,27 +32,47 @@ test('Test select table without selected field', () => {
 test('Test select table selected field', () => {
   const qb = new QueryBuilder('mysql');
   expect(qb.table('users').select('id', 'name').toRawSQL()).toBe(
-    'SELECT `id`,`name` FROM `users`;'
+    'SELECT `id`,`name` FROM `users`;',
   );
 });
 
 test('Test select table selected field with limit', () => {
   const qb = new QueryBuilder('mysql');
   expect(qb.table('users').select('id', 'name').limit(10).toRawSQL()).toBe(
-    'SELECT `id`,`name` FROM `users` LIMIT 10;'
+    'SELECT `id`,`name` FROM `users` LIMIT 10;',
   );
+});
+
+test('Test select table selected field with limit with offset', () => {
+  expect(
+    new QueryBuilder('mysql')
+      .table('users')
+      .select('id', 'name')
+      .limit(10)
+      .offset(5)
+      .toRawSQL(),
+  ).toBe('SELECT `id`,`name` FROM `users` LIMIT 5,10;');
+
+  expect(
+    new QueryBuilder('postgre')
+      .table('users')
+      .select('id', 'name')
+      .limit(10)
+      .offset(5)
+      .toRawSQL(),
+  ).toBe('SELECT "id","name" FROM "users" LIMIT 10 OFFSET 5;');
 });
 
 test('Delete table record with where', () => {
   const qb = new QueryBuilder('mysql');
   expect(qb.table('users').where({ id: 5 }).delete().toRawSQL()).toBe(
-    'DELETE FROM `users` WHERE `id`=5;'
+    'DELETE FROM `users` WHERE `id`=5;',
   );
 });
 
 test('Insert table', () => {
   const qb = new QueryBuilder('mysql');
   expect(
-    qb.table('users').insert({ username: 'visal', age: 5 }).toRawSQL()
+    qb.table('users').insert({ username: 'visal', age: 5 }).toRawSQL(),
   ).toBe("INSERT INTO `users`(`username`, `age`) VALUES('visal', 5);");
 });
